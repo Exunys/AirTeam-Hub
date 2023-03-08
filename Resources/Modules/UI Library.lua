4290b902eb1029cfd7031a256b95c2393d7642c9
Aleksandar <aleksandar.exunys@gmail.com>
2023-03-08 01:36:50 +0100

    Update UI Library.lua

--- Resources/Modules/UI Library.lua
+++ Resources/Modules/UI Library.lua
@@ -1,3 +1,5 @@
+local loadedGame = "Universal"
+
 --// CUSTOM DRAWING
 
 local drawing = {} do
@@ -1211,7 +1213,7 @@ function library:SaveConfig(name, universal)
         assert(self.extension, "No file extension specified")
 
         local configtbl = {}
-        local placeid = universal and "Universal" or game.PlaceId
+        local placeid = universal and "Universal" or loadedGame
 
         for flag, _ in next, flags do
             if not table.find(configignores, flag) then
@@ -1249,7 +1251,7 @@ function library:DeleteConfig(name, universal)
     assert(self.folder, "No folder specified")
     assert(self.extension, "No file extension specified")
 
-    local placeid = universal and "Universal" or game.PlaceId
+    local placeid = universal and "Universal" or loadedGame
 
     local folderpath = string.format("%s//%s", self.folder, placeid)
     local filepath = string.format("%s//%s.%s", folderpath, name, self.extension)
@@ -1264,7 +1266,7 @@ function library:LoadConfig(name, universal)
         assert(self.folder, "No folder specified")
         assert(self.extension, "No file extension specified")
 
-        local placeid = universal and "Universal" or game.PlaceId
+        local placeid = universal and "Universal" or loadedGame
 
         local folderpath = string.format("%s//%s", self.folder, placeid)
         local filepath = string.format("%s//%s.%s", folderpath, name, self.extension)
@@ -1288,7 +1290,7 @@ function library:GetConfigs(universal)
     assert(self.extension, "No file extension specified")
 
     local configs = {}
-    local placeidfolder = string.format("%s//%s", self.folder, game.PlaceId)
+    local placeidfolder = string.format("%s//%s", self.folder, loadedGame)
     local universalfolder = self.folder .. "//universal"
 
     for _, config in next, (isfolder(placeidfolder) and listfiles(placeidfolder) or {}) do
@@ -1357,7 +1359,7 @@ function library:SetTheme(theme)
         assert(self.extension, "No file extension specified")
 
         local folderpath = string.format("%s//themes", self.folder)
-        local filepath = string.format("%s//%s.json", folderpath, theme)
+        local filepath = string.format("%s//%s.cfg", folderpath, theme)
 
         if isfolder(folderpath) and isfile(filepath) then
             local themetbl = services.HttpService:JSONDecode(readfile(filepath))
@@ -1379,7 +1381,7 @@ function library:GetThemes()
     if isfolder(folderpath) then
         for _, theme in next, listfiles(folderpath) do
             local name = theme:gsub(folderpath .. "\\", "")
-            name = name:gsub(".json", "")
+            name = name:gsub(".cfg", "")
             table.insert(themes, name)
         end
     end
@@ -1408,7 +1410,7 @@ function library:SaveCustomTheme(name)
             makefolder(folderpath) 
         end
 
-        local filepath = string.format("%s//%s.json", folderpath, name)
+        local filepath = string.format("%s//%s.cfg", folderpath, name)
         writefile(filepath, theme)
 
         return true
@@ -2711,6 +2713,10 @@ function library.createkeybind(default, parent, blacklist, flag, callback, offse
     end
 
     return keybindtypes
+end
+
+function library:SetGameName(str)
+    loadedGame = str 
 end
 
 function library:Watermark(str)
